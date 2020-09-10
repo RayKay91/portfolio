c837b098c08bbf8aa72395a296fc5fc3c6c9021f
Rayhaan Khalid <rkhalid@me.com>
2020-09-10 14:14:10 +0100

    update gulp workflow, change styling, add skills

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,46 @@
-const gulp = require('gulp');
+const { src, dest, watch, series, parallel } = require('gulp');
 const htmlmin = require('gulp-htmlmin');
 const imagemin = require('gulp-imagemin');
 const cleanCSS = require('gulp-clean-css');
 const sass = require('gulp-sass');
 
 //Copy HTML files to dist folder
-gulp.task('minifyHTML', function () {
-  return gulp
-    .src('src/*.html')
+function minifyHTML() {
+  return src('src/*.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('dist'));
-});
+    .pipe(dest('dist'));
+}
 
 //optimise images
-gulp.task('optimiseImgs', function () {
-  return gulp
-    .src('src/images/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/images'));
-});
+function optimiseImgs() {
+  return src('src/images/*').pipe(imagemin()).pipe(dest('dist/images'));
+}
 
 //copy JS
 
-gulp.task('copy-js', function () {
-  return gulp.src('src/*.js').pipe(gulp.dest('dist'));
-});
+function copyJs() {
+  return src('src/*.js').pipe(dest('dist'));
+}
 
 //minify CSS
 
-gulp.task('minify-css', function () {
-  return gulp.src('src/*.css').pipe(cleanCSS()).pipe(gulp.dest('dist'));
-});
+function minifyCss() {
+  return src('src/*.css').pipe(cleanCSS()).pipe(dest('dist'));
+}
 
 //sass compiler
-gulp.task('compile-sass', function () {
-  return gulp
-    .src('src/*.scss')
+function compileSass() {
+  return src('src/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('src'));
-});
+    .pipe(dest('src'));
+}
 
-gulp.task(
-  'default',
-  gulp.series('minifyHTML', 'optimiseImgs', 'compile-sass', 'minify-css')
+function watchTask() {
+  watch(['src/styles.scss'], compileSass);
+}
+
+exports.default = series(
+  parallel(minifyHTML, optimiseImgs, copyJs, compileSass),
+  minifyCss,
+  watchTask
 );
-
-gulp.task('watch', function () {
-  gulp.watch('src/styles.scss').on('change', gulp.series('compile-sass'));
-});
